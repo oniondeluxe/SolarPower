e6713785bf63e0727a71565646e0825835fbb396
oniondeluxe <daniel.m.zagar@gmail.com>
2025-02-05 20:12:16 +0100

    Boilerplate project creation

---
+++ SolPwr.DomainModel/BusinessObjects/PowerPlant.cs
@@ -0,0 +1,16 @@
+﻿using OnionDlx.SolPwr.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionDlx.SolPwr.BusinessObjects
+{
+    public class PowerPlant : BusinessObject
+    {
+        public DateTime UtcInstallDate { get; set; }
+
+        public GeoCoordinate Location { get; set; }
+    }
+}
