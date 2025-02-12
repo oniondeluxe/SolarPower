b38be89f020a9c0b13ef3ce3da3cd0bdc5054d47
Daniel Zagar <daniel.m.zagar@gmail.com>
2025-02-12 23:13:17 +0100

    Added Unit Test (#4)
    * Br1
    
    * Added BL project
    
    * New project and separation started
    
    * ApiResponse
    
    * Split into factory and repo
    
    * Query works
    
    * Stub for CommandResult
    
    * Savepoint
    
    * Refac done except prognosis
    
    * Forgotten authorize
    
    * IQueryable extension method in provider layer. Auch
    
    * Added test project
    
    * Renamed file
    
    * Added Moq
    
    * Embryo
    
    * Reverse merge (#3)
    
    * Separation (#2)
    
    * Br1
    
    * Added BL project
    
    * New project and separation started
    
    * ApiResponse
    
    * Split into factory and repo
    
    * Query works
    
    * Stub for CommandResult
    
    * Savepoint
    
    * Refac done except prognosis
    
    * Forgotten authorize
    
    * IQueryable extension method in provider layer. Auch
    
    * Added test project
    
    * Renamed file
    
    * Added Moq
    
    * Removed hard coded path + comments
    
    * Wired up some Moq wrappers and utilities

--- SolPwr.BusinessLogic.Test/QualityAssurance/PowerPlantCrudTest.cs
+++ SolPwr.BusinessLogic.Test/QualityAssurance/PowerPlantCrudTest.cs
@@ -1,22 +1,94 @@
-﻿namespace OnionDlx.SolPwr.QualityAssurance
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using OnionDlx.SolPwr.BusinessLogic;
+using OnionDlx.SolPwr.BusinessObjects;
+using OnionDlx.SolPwr.Data;
+using OnionDlx.SolPwr.Services;
+using System.Collections;
+using System.Linq.Expressions;
+
+namespace OnionDlx.SolPwr.QualityAssurance
 {
     public class CrudTests
     {
+        #region Boilerplate
+
+        private Mock<IMeteoLookupService> _endpointMock;
+        private Mock<ILogger<IPlantManagementService>> _loggerMock;
+        private Mock<IMeteoLookupServiceCallback> _meteoCallbackMock;
+
+
         [SetUp]
         public void Setup()
         {
+            // Trivial parts are always the same here
+            _loggerMock = new Mock<ILogger<IPlantManagementService>>();
+            _endpointMock = new Mock<IMeteoLookupService>();
+            _meteoCallbackMock = new Mock<IMeteoLookupServiceCallback>();
+            _meteoCallbackMock.Setup(ep => ep.GetEndpoint()).Returns(_endpointMock.Object);
         }
 
+
+        private PlantManagementService CreateInstanceWithData(IList<PowerPlant> source)
+        {
+            // EF part
+            var repoFactoryMock = new Mock<IUtilitiesRepositoryFactory>();
+            var repoMock = new Mock<IUtilitiesRepository>();
+            repoFactoryMock.Setup(mock => mock.NewCommand()).Returns(repoMock.Object);
+            repoFactoryMock.Setup(mock => mock.NewQuery()).Returns(repoMock.Object);
+
+            var powerPlantCollectionMock = new Mock<IBusinessObjectCollection<PowerPlant>>().WithAsyncData(source);
+            repoMock.Setup(repo => repo.PowerPlants).Returns(powerPlantCollectionMock.Object);
+
+            // Service ready to be used
+            return new PlantManagementService(repoFactoryMock.Object, _loggerMock.Object, _meteoCallbackMock.Object);
+        }
+
+        #endregion
 
         [Test]
-        public void BlankRepo_CreatePlant_Success()
+        public async Task PrePopulated_SelectAll_Success()
         {
-            // TODO
-            // The following needs to be mocked:
-            // IUtilitiesRepositoryFactory - emulating EntityFramework layer
-            // IMeteoLookupServiceCallback - emulating the meteo integrator layer
+            // Arrange
+            var source = new List<PowerPlant>
+            {
+                new PowerPlant
+                {
+                    Id = Guid.NewGuid(),
+                    PlantName = "Three Mile island",
+                    UtcInstallDate = DateTime.UtcNow,
+                    Location = new GeoCoordinate(5, 10),
+                    PowerCapacity = 100
+                },
+                new PowerPlant
+                {
+                    Id = Guid.NewGuid(),
+                    PlantName = "福島第",
+                    UtcInstallDate = DateTime.UtcNow,
+                    Location = new GeoCoordinate(10, 20),
+                    PowerCapacity = 200
+                },
+                new PowerPlant
+                {
+                    Id = Guid.NewGuid(),
+                    PlantName = "Чорнобиль",
+                    UtcInstallDate = DateTime.UtcNow,
+                    Location = new GeoCoordinate(20, 30),
+                    PowerCapacity = 300
+                }
+            };
 
-            Assert.Inconclusive("Implementation is needed");
+            // This is the real service, but with mocked surroundings
+            var testee = CreateInstanceWithData(source);
+
+            // Act
+            var result = await testee.GetAllPlantsAsync();
+
+            // Assert
+            Assert.That(result.Count, Is.EqualTo(source.Count));
+            //_loggerMock.VerifyFor("OK");
         }
+
+
     }
 }
